24c18ac361e786ccece8e739595b8700b7911990
Chris <46506838+MotorcycleCapitalist@users.noreply.github.com>
2022-06-13 08:31:23 -0600

    created basic name function

---
+++ hello.js
@@ -0,0 +1,5 @@
+const sayHello = function(name) {  //declaration of the function w name
+  console.log('Hello ' + name );  //what the function does
+}
+
+sayHello('Miranda');  //call the function and the name to be used
